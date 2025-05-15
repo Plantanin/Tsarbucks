6e517869225ab0ef11f4e21f6a865f00b7382384
Florian Thubin <florianthubin@MacBook-Air-de-Florian.local>
2025-05-15 14:29:10 +0200

    ajot page selection boisson

--- src/main/java/com/tsarbuck/tsarbucks/HelloApplication.java
+++ src/main/java/com/tsarbuck/tsarbucks/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Launch_Hs.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/views/ChoixBoisson.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
